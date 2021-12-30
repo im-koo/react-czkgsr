74da05c9d190aea4491e726f239607d03a2778a9
im-koo <le.wokoko@gmail.com>
2021-12-30 20:20:40 +0530

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1 className="text-center">Hello Stack <span className="text-primary">Blitz!</span></h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
